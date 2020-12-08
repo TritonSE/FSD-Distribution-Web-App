f60dc9c980d9bb2ac199d3d2d1988c70d7337ba5
NirmalAgnihotri <53363382+NirmalAgnihotri@users.noreply.github.com>
2020-12-07 18:59:00 -0800

    rough navbar

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,13 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Agency',
+        url: '#',
+        cName: 'nav-links'
+    },
+    
+]

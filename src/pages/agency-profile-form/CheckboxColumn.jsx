5eeb9917dda6500ad366270cabf6f4c2ed7ceeda
wmwuu <wmwu@ucsd.edu>
2020-12-21 10:55:01 -0800

    Add custom checkboxes

--- src/pages/agency-profile-form/CheckboxColumn.jsx
+++ src/pages/agency-profile-form/CheckboxColumn.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
+import Checkbox from "./Checkbox";
 import "./formstyle.css";
 
 export default function CheckboxColumn(props) {
@@ -7,13 +8,7 @@ export default function CheckboxColumn(props) {
     <div className="checkbox-list-column selection-choice">
       {props.options.map((optionText, index) => {
         return (
-          <Form.Check
-            type="checkbox"
-            key={index}
-            id={optionText}
-            label={optionText}
-            custom
-          />
+          <Checkbox key={index} label={optionText} />
         );
       })}
     </div>

0d90eda8d5143eb0151c571cf50db60a899bdecc
wllmwu <wmwu@ucsd.edu>
2021-02-05 19:17:41 -0800

    Fix CreateAgencyBtn styling

--- frontend/src/components/CreateAgencyBtn/CreateAgencyBtn.js
+++ frontend/src/components/CreateAgencyBtn/CreateAgencyBtn.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
 import { useHistory } from "react-router-dom";
-import './CreateAgencyBtn.css';
+import "./CreateAgencyBtn.css";
 
 function CreateAgencyBtn() {
   let history = useHistory();
-  
+
   function handleClick() {
     history.push("/create-agency");
   }
-  
+
   return (
     <button type="button" onClick={handleClick}>
-      <div className="res-circle"><strong>+</strong></div>
+      <div className="res-circle">
+        <strong>+</strong>
+      </div>
       <span>Create New Agency Profile</span>
     </button>
   );

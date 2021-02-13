0bf20d84e644d03a1f0148ce6bc53c8db00a2e8e
StephenTan12 <sttan@ucsd.edu>
2021-02-13 15:09:37 -0800

    updated login route

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const passport = require('passport');
-const { body, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
-router.post('/login', passport.authenticate('local', {
-    failureRedirect: '/login'
-}), (req, res) => {
-    res.redirect('/');
-});
+const { isValidated } = require('../middleware/validation');
+require('dotenv').config();
+
+router.post('/login', passport.authenticate('local', { session: false }), 
+    function (req, res, next) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        res.status(200).json({
+            user: req.user,
+            token: jwt.sign(req.user.toJSON(), process.env.SECRET_KEY)
+        });
+    }
+);
 
 module.exports = router;

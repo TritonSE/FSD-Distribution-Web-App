008f48cb55d71218c1f722f3abed526c851b2484
StephenTan12 <sttan@ucsd.edu>
2021-02-14 19:33:58 -0800

    added form validation

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -6,13 +6,10 @@ const router = express.Router();
 const { isValidated } = require('../middleware/validation');
 require('dotenv').config();
 
-router.post('/login', passport.authenticate('local', { session: false }), 
-    function (req, res, next) {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-
+router.post('/', [
+    isValidated,
+    passport.authenticate('local', { session: false })
+],  function (req, res, next) {
         res.status(200).json({
             user: req.user,
             token: jwt.sign(req.user.toJSON(), process.env.SECRET_KEY)

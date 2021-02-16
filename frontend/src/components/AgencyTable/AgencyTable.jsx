26140926a403d798f8b3360559e35b70d5a35b20
ryanbuui <57205440+ryanbuui@users.noreply.github.com>
2021-02-16 12:48:40 -0800

    Fixed some styling issues on CreateAgencyBtn and Navbar components

---
+++ frontend/src/components/AgencyTable/AgencyTable.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Agency } from '../../../../backend/models'
+
+function AgencyTable() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AgencyTable;

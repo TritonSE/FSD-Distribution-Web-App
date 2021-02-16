f7d39c5a6df2a6a669d9a7ba7c40a1ada4abbd74
ryanbuui <57205440+ryanbuui@users.noreply.github.com>
2021-02-16 13:13:27 -0800

    Setup basic table component

--- frontend/src/components/AgencyTable/AgencyTable.jsx
+++ frontend/src/components/AgencyTable/AgencyTable.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import { Agency } from '../../../../backend/models'
+import React, { useState, useEffect } from 'react';
+import DataTable from "./DataTable";
 
 function AgencyTable() {
+  const [data, setData] = useState([]);
+  const [qFilter, setQFilter] = useState("");
+
+  useEffect(() => {
+    fetch("")
+    .then((res) => res.json())
+    .then((json) => setData(json));
+  }, []);
+
   return (
-    <div>
-      
+    <div className="agency-table">
+      <div id="filter"></div>
+      <div className="data-table-container">
+        <DataTable data={data} />
+      </div>
     </div>
   )
 }
---
+++ frontend/src/components/AgencyTable/DataTable.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function DataTable({ data }) {
+  return (
+    <table cellPadding={0} cellSpacing={0}>
+      <thead>
+        
+      </thead>
+    </table>
+  )
+}
+
+export default DataTable;

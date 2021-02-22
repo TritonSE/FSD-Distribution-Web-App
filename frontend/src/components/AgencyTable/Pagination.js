da6bb2e9d97bb3d3df10f82a8241310c6d94fe66
ryanbuui <57205440+ryanbuui@users.noreply.github.com>
2021-02-22 12:21:25 -0800

    Updated Agency Table and styled

--- frontend/src/components/AgencyTable/Pagination.js
+++ frontend/src/components/AgencyTable/Pagination.js
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './Pagination.css';
 
 const Pagination = ({currentPage, totalEntries, entriesPerPage, paginate}) => {
-    const previousPageText = '<';
-    const nextPageText = '>';
-    // for(let i = 1; i <= Math.ceil(totalEntries / entriesPerPage); ++i){
-    //     pageNumbers.push(i);
-    // }
-    const totalPages = Math.ceil(totalEntries/entriesPerPage);
-    return (
-        <div>
-            <button onClick = {() => paginate(--currentPage)} disabled = {currentPage <= 1}> {previousPageText} </button>
-            <p>{currentPage}</p>
-            <button onClick = {() => paginate(++currentPage)} disabled = {currentPage >= totalPages}> {nextPageText} </button>
-        </div>
-    )
+  const previousPageText = '<';
+  const nextPageText = '>';
+  // for(let i = 1; i <= Math.ceil(totalEntries / entriesPerPage); ++i){
+  //     pageNumbers.push(i);
+  // }
+  const totalPages = Math.ceil(totalEntries/entriesPerPage);
+  return (
+    <div className="pagination">
+      <button onClick = {() => paginate(--currentPage)} disabled = {currentPage <= 1}> {previousPageText} </button>
+      <p>{currentPage}</p>
+      <button onClick = {() => paginate(++currentPage)} disabled = {currentPage >= totalPages}> {nextPageText} </button>
+    </div>
+  )
 }
 
 export default Pagination
--- frontend/src/pages/Agency/Agency.js
+++ frontend/src/pages/Agency/Agency.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import CreateAgencyBtn from "../../components/CreateAgencyBtn/CreateAgencyBtn";
 import Pagination from "../../components/AgencyTable/Pagination";
 import DataTable from "../../components/AgencyTable/DataTable";
+import './Agency.css';
 
 let fOptions = {
   search: '',
@@ -40,28 +41,50 @@ const paginate = (pageNumber) => setCurrentPage(pageNumber);
   return (
     <div className="agency-table">
       <div className="search-container">
-        <input type="text" value={filters.search} onChange={(e) => {setFilter({
+      <label for="search" id="searchLabel">Search:</label>
+        <input id="search" type="text" value={filters.search} onChange={(e) => {setFilter({
           ...filters,
           search: e.target.value.toLowerCase(),
         }); paginate(1)}} />
       </div>
-      <div id="filter">
-        <select id="status" name="status" onChange={(e) => {setFilter({
-          ...filters,
-          status: e.target.value.toLowerCase(),
-        }); paginate(1)}}>
-          <option value="">None</option>
-          <option value="Onboarding">Onboarding</option>
-          <option value="Inactive">Inactive</option>
-          <option value="Active">Active</option>
-          <option value="On hold">On hold</option>
-        </select>
+      <div className="filter-container">
+        <h2>Sort By:</h2>
+        <div className="selects-container">
+          <select name="region">
+            <option value="">Region</option>
+          </select>
+          <select id="status" name="status" onChange={(e) => {setFilter({
+            ...filters,
+            status: e.target.value.toLowerCase(),
+          }); paginate(1)}}>
+            <option value="">None</option>
+            <option value="Onboarding">Onboarding</option>
+            <option value="Inactive">Inactive</option>
+            <option value="Active">Active</option>
+            <option value="On hold">On hold</option>
+          </select>
+          <select name="staff">
+            <option value="">Staff</option>
+          </select>
+          <select name="joined">
+            <option value="">Joined In</option>
+          </select>
+          <select name="storage">
+            <option value="">Storage</option>
+          </select>
+          <select name="transportation">
+            <option value="">Transportation</option>
+          </select>
+        </div>
+        <button id="clearall">Clear All</button>
       </div>
       <div className="data-table-container">
         <DataTable data={filtered.slice(indexOfFirstEntry, indexOfLastEntry)} />
         <Pagination currentPage = {currentPage} totalEntries = {filtered.length} entriesPerPage = {entriesPerPage} paginate={paginate}></Pagination>
       </div>
-      <CreateAgencyBtn />
+      <div className="create-btn-container">
+        <CreateAgencyBtn />
+      </div>
     </div>
   )
 }

330829faa0f3d04f517a4947f80ddf3dd007ec8a
NirmalAgnihotri <53363382+NirmalAgnihotri@users.noreply.github.com>
2021-02-22 02:53:37 -0800

    added pagination

--- frontend/src/pages/Agency/Agency.js
+++ frontend/src/pages/Agency/Agency.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import CreateAgencyBtn from "../../components/CreateAgencyBtn/CreateAgencyBtn";
+import Pagination from "../../components/AgencyTable/Pagination";
 import DataTable from "../../components/AgencyTable/DataTable";
 
 let fOptions = {
@@ -10,6 +11,8 @@ let fOptions = {
 function AgencyTable() {
   const [data, setData] = useState([]);
   const [filters, setFilter] = useState(fOptions);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [entriesPerPage] = useState(2);
 
   useEffect(() => {
     fetch('http://localhost:8000/agency/', { method: 'GET' })
@@ -21,7 +24,6 @@ function AgencyTable() {
   }, []);
 
   function search(rows) {
-    const cols = rows[0] && Object.keys(rows[0].tableContent);
     return rows.filter(
       (row) => 
         (row.tableContent.name.toLowerCase().indexOf(filters.search) > -1 ||
@@ -30,19 +32,24 @@ function AgencyTable() {
     );
 };
 
+const indexOfLastEntry = currentPage * entriesPerPage;
+const indexOfFirstEntry = indexOfLastEntry - entriesPerPage;
+//const currentPosts = data.slice(indexOfFirstEntry, indexOfLastEntry);
+const filtered = search(data);
+const paginate = (pageNumber) => setCurrentPage(pageNumber);
   return (
     <div className="agency-table">
       <div className="search-container">
-        <input type="text" value={filters.search} onChange={(e) => setFilter({
+        <input type="text" value={filters.search} onChange={(e) => {setFilter({
           ...filters,
           search: e.target.value.toLowerCase(),
-        })} />
+        }); paginate(1)}} />
       </div>
       <div id="filter">
-        <select id="status" name="status" onChange={(e) => setFilter({
+        <select id="status" name="status" onChange={(e) => {setFilter({
           ...filters,
           status: e.target.value.toLowerCase(),
-        })}>
+        }); paginate(1)}}>
           <option value="">None</option>
           <option value="Onboarding">Onboarding</option>
           <option value="Inactive">Inactive</option>
@@ -51,7 +58,8 @@ function AgencyTable() {
         </select>
       </div>
       <div className="data-table-container">
-        <DataTable data={search(data)} />
+        <DataTable data={filtered.slice(indexOfFirstEntry, indexOfLastEntry)} />
+        <Pagination currentPage = {currentPage} totalEntries = {filtered.length} entriesPerPage = {entriesPerPage} paginate={paginate}></Pagination>
       </div>
       <CreateAgencyBtn />
     </div>

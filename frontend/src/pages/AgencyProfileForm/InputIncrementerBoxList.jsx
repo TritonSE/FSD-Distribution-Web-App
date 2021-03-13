b3afa53272ce70b63bcd0234d9bb1b89e4d42dc5
wllmwu <74469686+wllmwu@users.noreply.github.com>
2021-03-12 22:29:13 -0800

    Feature/emmanuelclflores/1036702205 update dist section (#22)
    * Added custom calendar component
    
    * Made minor schema updates
    
    * Update schema distribution section
    
    * Update form state
    
    * Prevent wrapping when window is small
    
    * Fix class headers
    
    * Make checkboxes call handler directly
    
    * Make incrementer boxes call handler directly
    
    * Begin implementing DistributionDays
    
    * Refactored custom calendar component
    
    * Integrated calendar with form and form state
    
    * Refactor calendar functions, state, file names
    
    * Remove selected/excluded dates when overwritten by weekday selection
    
    * Fix alignments/styling of distribution section components
    
    * Begin implementing Timebox
    
    * Implement+style TimeBox
    
    * Redo changes to backend
    
    * Add distribution start time validators
    
    * Highlight invalid start times
    
    * Update retail rescue section + schema
    
    * Make label optional on InputCheckboxList
    
    * Added method/class headers for distribution calendar
    
    * Added method/class headers for distribution calendar
    
    * Rename+update test file for AgencyProfileForm
    
    * Correct filename case
    
    * Add doc comments for agency.js
    
    * Add comments to AgencyProfileForm.js
    
    * Handle non-number frequency in Calendar
    
    * Resolved pull request comments
    
    Co-authored-by: Emmanuel Flores <ecflores@ucsd.edu>

--- frontend/src/pages/AgencyProfileForm/InputIncrementerBoxList.jsx
+++ frontend/src/pages/AgencyProfileForm/InputIncrementerBoxList.jsx
@@ -11,49 +11,40 @@ import "./FormStyle.css";
  *
  * Expected props:
  * - {String} label: label to display above the whole incrementer box group
+ * - {String} subLabel: subheading to display
  * - {Array<Object>} options: list of objects containing data about each
- * numeric option (title, current value, and the sub-key to use in the onChange
- * callback)
- * - {String} stateKey: first part of the key to pass into the onChange callback
+ * numeric option (title, current value, and the state key to use in the
+ * onChange callback)
  * - {Function} onChange: callback to handle input changes, should take a String
  * and a Number
  * - {Boolean} twoColumns: whether the incrementer box group should be split
  * into two columns
  */
 class InputIncrementerBoxList extends Component {
-  /**
-   * Callback for handling when the user changes a number. Passes it up to
-   * the callback from the form page.
-   * @param {Number} index Index (in options) of the number that was changed
-   */
-  onChange = (index, newNumber) => {
-    const { options, onChange } = this.props;
-    onChange(options[index].stateKey, newNumber);
-  };
-
   render() {
+    const { twoColumns, label, subLabel, options, onChange } = this.props;
     let listColumns = null;
-    if (this.props.twoColumns) {
-      let numOptions = this.props.options.length;
+    if (twoColumns) {
+      let numOptions = options.length;
       let midIndex = numOptions / 2; // index of first thing in second column
       if (numOptions % 2 === 1) midIndex++;
 
-      let firstColumnOptions = this.props.options.slice(0, midIndex);
-      let secondColumnOptions = this.props.options.slice(midIndex); // to end
+      let firstColumnOptions = options.slice(0, midIndex);
+      let secondColumnOptions = options.slice(midIndex); // to end
       listColumns = (
         <FormRow>
           <FormCol>
             <IncrementerBoxColumn
               options={firstColumnOptions}
               indexBuffer={0}
-              onChange={this.onChange}
+              onChange={onChange}
             />
           </FormCol>
           <FormCol>
             <IncrementerBoxColumn
               options={secondColumnOptions}
               indexBuffer={firstColumnOptions.length}
-              onChange={this.onChange}
+              onChange={onChange}
             />
           </FormCol>
         </FormRow>
@@ -61,9 +52,9 @@ class InputIncrementerBoxList extends Component {
     } else {
       listColumns = (
         <IncrementerBoxColumn
-          options={this.props.options}
+          options={options}
           indexBuffer={0}
-          onChange={this.onChange}
+          onChange={onChange}
         />
       );
     }
@@ -71,10 +62,8 @@ class InputIncrementerBoxList extends Component {
     return (
       <div className="form-input">
         <label className="form-input-list-header">
-          <span>{this.props.label + " "}</span>
-          <span className="form-input-list-subheader">
-            {this.props.subLabel}
-          </span>
+          <span>{label + " "}</span>
+          <span className="form-input-list-subheader">{subLabel}</span>
         </label>
         {listColumns}
       </div>

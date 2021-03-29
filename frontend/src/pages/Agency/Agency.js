36084196d7560acff7027516d8ac743173fd1884
NirmalAgnihotri <53363382+NirmalAgnihotri@users.noreply.github.com>
2021-03-29 15:48:23 -0700

    changed to 20 per page

--- frontend/src/pages/Agency/Agency.js
+++ frontend/src/pages/Agency/Agency.js
@@ -59,7 +59,7 @@ function AgencyTable() {
   const [data, setData] = useState([]);
   const [filters, setFilter] = useState(fOptions);
   const [currentPage, setCurrentPage] = useState(1);
-  const [entriesPerPage] = useState(2);
+  const [entriesPerPage] = useState(20);
   const [selected, setSelected] = useState({});
 
   useEffect(() => {

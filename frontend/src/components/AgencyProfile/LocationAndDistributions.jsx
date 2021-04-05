1286eca8f2535e9dd5f49fbb0302593537ba5a9e
ryanbuui <57205440+ryanbuui@users.noreply.github.com>
2021-04-04 19:19:09 -0700

    Added LocationAndDistributions component

--- frontend/src/components/AgencyProfile/LocationAndDistributions.jsx
+++ frontend/src/components/AgencyProfile/LocationAndDistributions.jsx
@@ -9,45 +9,59 @@ function LocationAndDistributions({agency}) {
         <h1 className="category-title">
           LOCATION AND DISTRIBUTION HOURS
         </h1>
-        <p>
-          <strong>Region:</strong>
-          &nbsp;{agency.tableContent.region}
-        </p>
-        <p>
-          <strong>San Diego District:</strong>
-          &nbsp;{agency.sanDiegoDistrict} |
-          &nbsp;<strong>County District:</strong>
-          &nbsp;{agency.countyDistrict} |
-          &nbsp;<strong>State Assembly District:</strong>
-          &nbsp;{agency.stateAssemblyDistrict}
-        </p>
-        <p>
-          <strong>State Senate District:</strong>
-          &nbsp;{agency.stateSenateDistrict} |
-          &nbsp;<strong>Federal Congressional District:</strong>
-          &nbsp;{agency.federalCongressionalDistrict}
-          {"\n"}
-        </p>
-        <p>
-          <strong>Additional Site:</strong>
-          &nbsp;{agency.additionalAddresses.map((add, index) => {
-            if(agency.additionalAddresses.length-1 <= index) {
-              return add+""
-            } else {
-              return add+", "
-            }
-          })}
-        </p>
-        <p>
-          <strong>Billing Address and Zipcode:</strong>
-          &nbsp;{agency.billingAddress}, {agency.billingZipcode}
-        </p>
-        <p>
-          <strong>Distribution Type:</strong>
-          &nbsp;{(agency.pantry && !agency.mealProgram) && "Pantry"}
-          {(!agency.pantry && agency.mealProgram) && "Meal Program"}
-          {(agency.pantry && agency.mealProgram) && "Pantry, Meal Program"}
-        </p>
+        <div className="region-container">
+          <p>
+            <strong>Region:</strong>
+            &nbsp;{agency.tableContent.region}
+          </p>
+          <p>
+            <strong>San Diego District:</strong>
+            &nbsp;{agency.sanDiegoDistrict} |
+            &nbsp;<strong>County District:</strong>
+            &nbsp;{agency.countyDistrict} |
+            &nbsp;<strong>State Assembly District:</strong>
+            &nbsp;{agency.stateAssemblyDistrict}
+          </p>
+          <p>
+            <strong>State Senate District:</strong>
+            &nbsp;{agency.stateSenateDistrict} |
+            &nbsp;<strong>Federal Congressional District:</strong>
+            &nbsp;{agency.federalCongressionalDistrict}
+          </p>
+        </div>
+        <div className="address-container">
+          <p>
+            <strong>Additional Site:</strong>
+            &nbsp;{agency.additionalAddresses.map((add, index) => {
+              if(agency.additionalAddresses.length-1 <= index) {
+                return add+""
+              } else {
+                return add+", "
+              }
+            })}
+          </p>
+          <p>
+            <strong>Billing Address and Zipcode:</strong>
+            &nbsp;{agency.billingAddress}, {agency.billingZipcode}
+          </p>
+          <p>
+            <strong>Distribution Type:</strong>
+            &nbsp;{(agency.pantry && !agency.mealProgram) && "Pantry"}
+            {(!agency.pantry && agency.mealProgram) && "Meal Program"}
+            {(agency.pantry && agency.mealProgram) && "Pantry, Meal Program"}
+          </p>
+        </div>
+        <div className="distributions-container">
+          <div className="distributions-label">
+            <p>
+              <strong>Distribution Days:</strong>
+            </p>
+            <p>
+              <strong>Frequency: </strong>
+              &nbsp;{agency.distributionFrequency}
+            </p>
+          </div>
+        </div>
       </div>
     </>
   )

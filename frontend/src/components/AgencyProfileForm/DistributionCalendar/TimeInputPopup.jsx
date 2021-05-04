32fbd678f2f7ba7d284df15de61b0936f271d189
William Wu <dev.wwu@gmail.com>
2021-05-03 22:55:48 -0700

    Fix TimeInputPopup delete handling, implement hide on click outside

--- frontend/src/components/AgencyProfileForm/DistributionCalendar/TimeInputPopup.jsx
+++ frontend/src/components/AgencyProfileForm/DistributionCalendar/TimeInputPopup.jsx
@@ -18,14 +18,44 @@ import deleteIcon from "./trash-can.svg";
  * should take no arguments
  */
 class TimeInputPopup extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { shown: true };
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+  }
+
+  componentDidUpdate() {
+    setTimeout(() => {
+      if (this.state.shown) {
+        window.addEventListener("click", this.handleClickOutside);
+      } else {
+        window.removeEventListener("click", this.handleClickOutside);
+      }
+    }, 0); // 0 ms delay schedules this add/remove listener for next event loop
+  }
+
+  handleClickOutside(event) {
+    if (!event.target.closest(".calendar-time-popup")) {
+      this.setState({ shown: false });
+    }
+  }
+
   render() {
     const { value, valid, onChange, onDelete } = this.props;
 
+    if (!this.state.shown) {
+      return null;
+    }
     return (
       <div className="calendar-time-popup">
         <div>
           Start time for custom day:
-          <button type="button" className="delete-button" onClick={onDelete}>
+          <button
+            type="button"
+            className="delete-button"
+            onClick={() => this.setState({ shown: false }, onDelete)}
+          >
             <img src={deleteIcon} alt="delete" />
           </button>
         </div>
